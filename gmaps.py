6c79889bb7221d4cf8a539b1efcebafe26614ee5
marilymaril <mflorek@csumb.edu>
2016-05-09 14:02:57 -0700

    should be able to put in the starting & ending points as arguments now

--- gmaps.py
+++ gmaps.py
@@ -16,24 +16,25 @@ def get_destination():
     print(destination)
     return destination
 
-starting="Monterey,ca" #get_starting()
-destination="Hollister,ca" #get_destination()
+def directions(starting,destination):
+    #starting="Monterey,ca" #get_starting()
+    #destination="Hollister,ca" #get_destination()
 
-urllib.urlretrieve("https://maps.googleapis.com/maps/api/directions/json?origin="+starting+"&destination="+destination+"&key=AIzaSyDYEI9_pHILyfzxpL-vTSGLlrklfOlZzvc", "directions.json")
+    urllib.urlretrieve("https://maps.googleapis.com/maps/api/directions/json?origin="+starting+"&destination="+destination+"&key=AIzaSyDYEI9_pHILyfzxpL-vTSGLlrklfOlZzvc", "directions.json")
 
-with open('directions.json') as data_file:
-    data = json.load(data_file)
+    with open('directions.json') as data_file:
+        data = json.load(data_file)
 
 
-if data["status"]=="ZERO_RESULTS":
-    print("Invalid Address")
-else:
-    length = len(data['routes'][0]['legs'][0]['steps'])
+    if data["status"]=="ZERO_RESULTS":
+        print("Invalid Address")
+    else:
+        length = len(data['routes'][0]['legs'][0]['steps'])
 
-    for i in range(0,length):
-        directions = data['routes'][0]['legs'][0]['steps'][i]['html_instructions']
-        str(directions)
-        directions=directions.replace('</b>','')
-        directions = directions.replace('<b>','')
-        print(directions)
+        for i in range(0,length):
+            directions = data['routes'][0]['legs'][0]['steps'][i]['html_instructions']
+            str(directions)
+            directions=directions.replace('</b>','')
+            directions = directions.replace('<b>','')
+            return directions
 
