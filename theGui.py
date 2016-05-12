445a72d34f1927f235213ab7ac690bb10154bd2c
Louis Paul Romero <louiee831@gmail.com>
2016-05-12 12:56:55 -0700

    FINAL

--- theGui.py
+++ theGui.py
@@ -1,3 +1,15 @@
+'''GitHub link
+https://github.com/Louis-Romero/projectThree.git
+
+Team 94
+Christopher Forsythe
+Marilyn Florek
+Louis Romero
+
+Summary:  This program will grab a user's 10 upcoming events through the use of Google Calendar API and alert them through SMS using TWilio API.  
+Text based navigation has also been set up through the use of Google Maps Directions API and our messaging response system.  
+A GUI has also been set up for the developer to see how many text messages have been sent in 24 hours.'''
+
 from Tkinter import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
